e32e03813e982850303591082f2568a4ef16d6e6
Roman <b111forjob@gmail.com>
2020-02-22 23:02:01 +0700

    Different alphas and beutify on dirs

---
+++ SequentialBrutForce/LittleAlpha/ChunkMethod/seqBF-little.hs
@@ -0,0 +1,69 @@
+import Control.Parallel.Strategies
+import Crypto.Hash.SHA1 as SHA1
+import Control.DeepSeq
+import Control.Monad
+import System.Environment
+import Data.Maybe
+import Data.Char
+import Data.List.Split
+import qualified Data.ByteString.Lazy.UTF8 as LSU
+import qualified Data.ByteString.UTF8 as SSU
+import qualified Data.ByteString.Lazy as LStr
+--import Data.ByteString.Conversion
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Base16.Lazy as B16L
+
+alphabet :: String
+alphabet = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+
+--main = print $ SHA1.hashlazy (LStr.pack [0..255])
+
+--allPosPass = [ c : s | s <- "" : allPosPass, c <- alphabet] -- returns ALL possible passwords
+
+checkPass :: String -> SSU.ByteString -> Bool
+checkPass pass hash = let bString = SSU.fromString pass
+                          assumpt = SHA1.hash bString
+                      in if assumpt == hash then True else False
+
+solveSeq' :: SSU.ByteString -> [String] -> Maybe String
+solveSeq' _ [] = Nothing
+solveSeq' hash (str:strs) = if (checkPass str hash) then Just str else solveSeq' hash strs
+
+solveSeq :: SSU.ByteString -> [[String]] -> Maybe String 
+solveSeq _ []            = Nothing
+solveSeq hash (chunk:cs) = let res = solveSeq' hash chunk in
+                           if isJust res then res else solveSeq hash cs
+
+--splitToChunks :: Int -> [String]
+--splitToChunks chunkLen arr
+
+
+passOfLen n 
+  | n == 0    = "":[]
+  | n == 1    = [ [c] | c <- alphabet]
+  | otherwise = [ c:str | c <- alphabet, str <- passOfLen $ pred n] 
+
+passOfLenNM n
+  | n == 0           = passOfLen n
+  | otherwise        = passOfLen n ++ (passOfLenNM $ pred n)
+
+main = do
+  --putStrLn "Please insert your SHA-1 and I will return your password"
+  hash:numberOfChars':_ <- getArgs
+  let [(numberOfChars,_)] = reads numberOfChars' :: [(Int, String)]
+  let (hashBS, _) = (B16.decode.SSU.fromString) hash
+  let passwordSolve = solveSeq hashBS $ chunksOf 52000000 $ passOfLenNM numberOfChars
+  --let passwordSolve = filter (\x -> checkPass x hashBS) $ passOfLenNM numberOfChars
+  --if isNothing passwordSolve then putStrLn "Couldn't find a match, sorry!" else putStrLn $ fromJust passwordSolve
+  if isNothing passwordSolve 
+  then do 
+    putStrLn "\n----------------------FAILURE------------------------" 
+    putStrLn "\tWe could not find your password:("
+    putStrLn "----------------------+++++++------------------------\n"
+  else do 
+    putStrLn "\n----------------------SUCCESS------------------------"
+    putStrLn $ "\t\tYour password is " ++ (show $ fromJust passwordSolve)
+    putStrLn "----------------------+++++++------------------------\n"
+  --end <- getLine
+  return ()
+
