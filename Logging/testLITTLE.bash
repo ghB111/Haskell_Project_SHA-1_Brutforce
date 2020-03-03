fbdaaf99bfc2e611124678f5c4d63d4a1e106daa
Roman <b111forjob@gmail.com>
2020-03-03 12:57:02 +0700

    Testing update

--- Logging/testLITTLE.bash
+++ Logging/testLITTLE.bash
@@ -11,22 +11,22 @@ echo "Testing on \"$CODE\", hash \"$hash\""
 mkdir --parents "$LOGDIR/logs/little/"
 
 echo "Testing 4 threads..."
-echo "Test: Parallel on little alpha, password passed: $CODE, hash: \"$hash\"" > "$LOGDIR/logs/little/logALL.txt"
-echo "Params: maxPassLen: $LEN, chunk multiplier: $CH" >> "$LOGDIR/logs/little/logALL.txt"
-./parBF-little $hash $LEN $CH +RTS -s -N4 -ls &>> "$LOGDIR/logs/little/logALL.txt"
-mv parBF-little.eventlog "$LOGDIR/logs/little/parBF-ALL.eventlog"
+echo "Test: Parallel on little alpha, password passed: $CODE, hash: \"$hash\"" > "$LOGDIR/logs/little/log4.txt"
+echo "Params: maxPassLen: $LEN" >> "$LOGDIR/logs/little/log4.txt"
+./parBF-little $hash $LEN +RTS -s -N4 -ls &>> "$LOGDIR/logs/little/log4.txt"
+mv parBF-little.eventlog "$LOGDIR/logs/little/parBF-4.eventlog"
 echo "Done"; echo
 
 echo "Testing 2 threads..."
 echo "Test: Parallel on little alpha, password passed: $CODE, hash: \"$hash\"" > "$LOGDIR/logs/little/log2.txt"
-echo "Params: maxPassLen: $LEN, chunk multiplier: $CH"  >> "$LOGDIR/logs/little/log2.txt"
-./parBF-little $hash $LEN $CH +RTS -s -N2 -ls &>> "$LOGDIR/logs/little/log2.txt"
+echo "Params: maxPassLen: $LEN"  >> "$LOGDIR/logs/little/log2.txt"
+./parBF-little $hash $LEN +RTS -s -N2 -ls &>> "$LOGDIR/logs/little/log2.txt"
 mv parBF-little.eventlog "$LOGDIR/logs/little/parBF-2.eventlog"
 echo "Done"; echo
 
-echo "Testing 1 thread..."
-echo "Test: Parallel on little alpha, password passed: $CODE, hash: \"$hash\"" > "$LOGDIR/logs/little/log1.txt"
-echo "Params: maxPassLen: $LEN, chunk multiplier: $CH" >> "$LOGDIR/logs/little/log1.txt"
-./parBF-little $hash $LEN $CH +RTS -s -N1 -ls &>> "$LOGDIR/logs/little/log1.txt"
-mv parBF-little.eventlog "$LOGDIR/logs/little/parBF-1.eventlog"
+echo "Testing sequentially..."
+echo "Test: Sequential on little alpha, password passed: $CODE, hash: \"$hash\"" > "$LOGDIR/logs/little/logSEQ.txt"
+echo "Params: maxPassLen: $LEN" >> "$LOGDIR/logs/little/logSEQ.txt"
+./seqBF-little $hash $LEN +RTS -s &>> "$LOGDIR/logs/little/logSEQ.txt"
+#mv parBF-little.eventlog "$LOGDIR/logs/little/parBF-seq.eventlog"
 echo "Done!"; echo
